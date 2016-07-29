13010d02a99847a95611c20653dba4f945757c2c
Rodrigo Dumont <rlsdumont@gmail.com>
2016-07-29 11:56:13 -0300

    Fix Heroku port

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 
-const port = process.env.NODE_ENV === 'production' ? 80 : 3000
+const port = process.env.PORT || 3000
 let app = express()
 
 app.get('/', (req, res) => {
